ba054bdf88ec98e5f80d4925b1fa320f587556f5
Andres Rodriguez <andresrodriguez4@outlook.com.ar>
2020-04-30 04:55:19 -0300

    added html, css and basic js, also app is working

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# weather_app
-# weather_app
+# Weather App
+
+> A simple app that tells you the wether based on the provided city.
+
+![screenshot](./app_screenshot.png)
+
+Additional description about the project and its features.
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+- Webpack
+
+## Live Demo
+
+[Live Demo Link](https://livedemo.com)
+
+
+## Getting Started
+
+- Clone this project on your computer.
+- Open the files on you text editor.
+- Open the .html on your browser to see the future changes.
+
+### Prerequisites
+
+- Any text editor to inspect the code. 
+
+### Setup
+
+- Clone the project to your computer.
+- run: npm install.
+- run: npm run start.
+    
+### Usage
+
+## Authors
+
+👤 **Andres Rodriguez**
+
+- Github: [@githubhandle](https://github.com/githubhandle)
+- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
+- Linkedin: [linkedin](https://linkedin.com/linkedinhandle)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](lic.url) licensed.
---
+++ main.js
@@ -0,0 +1,59 @@
+const api = {
+    key: "4c4e39e4d19df4b03f3b39a7f44a5e0e",
+    base: "https://api.openweathermap.org/data/2.5/weather?q="
+}
+
+
+const searchbox = document.querySelector('.search-box');
+
+const getResult = (query) => {
+    fetch(`${api.base}${query}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+        return weather.json()})
+        .then(displayResults);
+}
+
+const displayResults = (weather) => {
+    console.log(weather)
+    let city = document.querySelector('.city')
+    city.innerHTML = `${weather.name}, ${weather.sys.country}`
+
+    let now = new Date();
+    let date = document.querySelector('.date')
+    date.innerHTML = dateBuilder(now)
+
+    let temp = document.querySelector('.temp')
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`
+
+    let weather_element = document.querySelector('.weather')
+    weather_element.innerHTML = `${weather.weather[0].main}`;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  };
+
+const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+  
+    return `${day} ${date} ${month} ${year}`;
+  }
+  
+
+const setQuery = (event) => {
+    if (event.keyCode == 13) {
+        getResult(searchbox.value);
+    }
+}
+
+searchbox.addEventListener('keypress', setQuery);
+
+
+
+
+
